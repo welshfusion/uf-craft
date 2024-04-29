0624c2e6f476ff43fd44c85e986f5a9e1ac33f3b
Amici Infotech <mufaddal.nala1@gmail.com>
2024-04-29 17:18:52 +0530

    add basicContent Block

--- config/colour-swatches.php
+++ config/colour-swatches.php
@@ -57,7 +57,25 @@ return [
                         'color' => '#000000',
                     ]
                 ]
-            ]
+            ],
+            [
+                'label'   => 'green',
+                'default' => false,
+                'color'   =>  [
+                    [
+                        'color' => '#9da012',
+                    ]
+                ]
+            ],
+            [
+                'label'   => 'cyprus',
+                'default' => false,
+                'color'   =>  [
+                    [
+                        'color' => '#003e44',
+                    ]
+                ]
+            ],
         ],
         'Pattern Color' => [
             [
