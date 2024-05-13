af3fff0ee0e520f3621a0c0282c1538d262cb5ac
Amici Infotech <mufaddal.nala1@gmail.com>
2024-05-13 17:55:06 +0530

    add  basic blocks

---
+++ config/formie.php
@@ -0,0 +1,23 @@
+<?php
+
+return [
+    'themeConfig' => [
+        "fieldInput" => [
+            "attributes" => [
+                "class" => 'peer !block !w-full !border !border-neutral-300 !bg-transparent !px-6 !pb-4 !pt-12 !text-base/6 !text-cyprus-500 !ring-4 !ring-transparent !transition focus:!border-neutral-950 focus:!outline-none focus:!ring-neutral-950/5 group-first:!rounded-t-2xl group-last:!rounded-b-2xl',
+            ],
+        ],
+
+        "fieldLabel" => [
+            "attributes" => [
+                "class" => '!pointer-events-none !absolute !left-6 !top-1/2 !-mt-3 !origin-left !text-base/6 !text-neutral-500 !transition-all !duration-200 peer-focus:-!translate-y-4 peer-!focus:scale-75 peer-focus:!font-semibold peer-focus:!text-cyprus-500 peer-[:not(:placeholder-shown)]:-!translate-y-4 peer-[:not(:placeholder-shown)]:!scale-75 peer-[:not(:placeholder-shown)]:!font-semibold peer-[:not(:placeholder-shown)]:!text-cyprus-500',
+            ],
+        ],
+
+        "submitButton" => [
+            "attributes" => [
+                "class" => '!mt-10 !inline-flex !rounded-full !px-4 !py-1.5 !text-sm !font-semibold !transition !bg-cyprus-500 !text-white hover:!bg-green-500',
+            ],
+        ],
+    ],
+];
